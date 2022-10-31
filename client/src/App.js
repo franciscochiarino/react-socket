983bb135b0348cbaa90aa4b924fdf2acdf0e67f7
Fran <francisco.chiarino@gmail.com>
2022-10-31 22:26:06 +0100

    Connect socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import io from 'socket.io-client';
+import { useEffect } from 'react';
+
+const socket = io.connect('http://localhost:3001');
 
 function App() {
+  const sendMessage = () => {
+    console.log('Sending message...')
+
+    socket.emit('send_message', { message: 'Hello' })
+  }
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      alert(data.message)
+    })
+  }, [socket]);
+
   return (
     <div className="App">
       <h1>Hello Fran</h1>
+      <input placeholder="Message..." />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
