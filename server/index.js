d169bb09f0932f1676f2cbf598ceeeeada3ea44f
Fran <francisco.chiarino@gmail.com>
2022-11-01 03:30:15 +0100

    Count connections
    Doesn't seem like the most pro way to do it

--- server/index.js
+++ server/index.js
@@ -15,9 +15,13 @@ const io = new Server(server, {
   }
 });
 
+let connections = 0;
+
 io.on('connection', (socket) => {
-  console.log(`User connected in ${socket.id}`)
-  socket.broadcast.emit('receive_initial_deck', [...Array(10).keys()]);
+  connections++
+  console.log(`User connected in ${socket.id}`);
+  console.log(connections);
+  socket.emit('receive_initial_deck', [...Array(10).keys()]);
 
   socket.on('send_deck', deck => {
     socket.broadcast.emit('receive_deck', deck);
