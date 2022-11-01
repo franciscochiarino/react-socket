036e349c26777f005ff972b3b2990ddc83758839
Fran <francisco.chiarino@gmail.com>
2022-11-01 03:16:34 +0100

    Set basic functionality

--- client/src/App.js
+++ client/src/App.js
@@ -8,38 +8,6 @@ import Home from './Home';
 import Room from './Room';
 
 function App() {
-  // const [turn, setTurn] = useState(true);
-  // const [deck, setDeck] = useState([]);
-
-  // const drawCard = () => {
-  //   deck.pop();
-
-  //   socket.emit('send_deck', deck);
-  //   setTurn(false);
-  // }
-
-  // const shuffleDeck = () => {
-  //   deck.sort(() => 0.5 - Math.random());
-
-  //   socket.emit('send_deck', deck);
-  //   setTurn(false);
-  // }
-
-  // useEffect(() => {
-  //   socket.on('receive_initial_deck', (deck) => {
-  //     setDeck(deck);
-  //     setTurn(true);
-  //   })
-
-  //   socket.on('receive_deck', deck => {
-  //     setDeck(deck);
-  //     setTurn(true);
-  //   })
-
-  //   if (!deck.length) {
-  //     setTurn(false)
-  //   }
-  // }, [socket]);
 
   return (
     <div className="App">
@@ -49,12 +17,6 @@ function App() {
           <Route path="/" element={<Home/>} />
         </Routes>
       </Router>
-
-      {/* <h4>{turn ? 'Go ahead' : 'Wait for your turn'}</h4>
-      {console.log(turn)}
-      <h3>{turn && deck}</h3>
-      <button onClick={drawCard} disabled={!turn}>Draw card</button>
-      <button onClick={shuffleDeck} disabled={!turn}>Shuffle</button> */}
     </div>
   );
 }
--- client/src/Room.js
+++ client/src/Room.js
@@ -4,6 +4,22 @@ import { useEffect, useState } from 'react';
 
 export default function Room() {
   const [socket, setSocket] = useState(null);
+  const [turn, setTurn] = useState(true);
+  const [deck, setDeck] = useState([]);
+
+  const drawCard = () => {
+    deck.pop();
+
+    socket.emit('send_deck', deck);
+    setTurn(false);
+  }
+
+  const shuffleDeck = () => {
+    deck.sort(() => 0.5 - Math.random());
+
+    socket.emit('send_deck', deck);
+    setTurn(false);
+  }
 
   useEffect(() => {
     const newSocket = io('http://localhost:3001');
@@ -11,6 +27,20 @@ export default function Room() {
     newSocket.on('connect', (socket) => {
       console.log('socket connected', newSocket);
     })
+
+    newSocket.on('receive_initial_deck', (deck) => {
+      setDeck(deck);
+      setTurn(true);
+    })
+
+    newSocket.on('receive_deck', deck => {
+      setDeck(deck);
+      setTurn(true);
+    })
+
+    if (!deck.length) {
+      setTurn(false)
+    }
     
     setSocket(newSocket);
     return () => newSocket.close();
@@ -19,6 +49,11 @@ export default function Room() {
   return (
     <div>
       <h1>Room</h1>
+      <h4>{turn ? 'Go ahead' : 'Wait for your turn'}</h4>
+      {console.log(turn)}
+      <h3>{turn && deck}</h3>
+      <button onClick={drawCard} disabled={!turn}>Draw card</button>
+      <button onClick={shuffleDeck} disabled={!turn}>Shuffle</button>
     </div>
   )
 }
